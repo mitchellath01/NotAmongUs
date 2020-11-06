9ffa4a1d7b664b22d4e76ac5e9faf10734169a04
MitchellAth - MBP <mmmmm@hotmail.com.au>
2020-11-06 23:31:44 +1100

    New Alibis work

--- NotAmongUs/GameFiles.h
+++ NotAmongUs/GameFiles.h
@@ -11,3 +11,5 @@ using namespace std;
 
 vector <Room*> gameRooms;
 vector <Character*> gameCharacters;
+const vector <string> characterNames = { "Adam", "Alex", "Aaron", "Ben", "Carl", "Dan", "David", "Edward", "Fred", "Frank", "George", "Hal", "Hank", "Ike", "John", "Jack", "Joe", "Larry", "Monte", "Matthew", "Mark", "Nathan", "Otto", "Paul", "Peter", "Roger", "Roger", "Steve", "Thomas", "Tim", "Ty", "Victor", "Walter" };
+const vector <string> standardAlibis = { "were doing a game of chess", "went on a quick midnight snack together", "were looking at the stars", "did some tiktok dances" };
--- NotAmongUs/GameSetup.cpp
+++ NotAmongUs/GameSetup.cpp
@@ -6,75 +6,97 @@ void startGame() {
 	system("cls");
 	printTitleBar("\tGame Starting");
 	//Test drive greating rooms
-	printSingleBar();
-	cout << "\tGame loading: Rooms";
+	//printSingleBar();
+	//cout << "\tGame loading: Rooms";
 
-	gameRooms.clear();
-	gameRooms.push_back(new Room(Bedroom));
-	gameRooms.push_back(new Room(Kitchen));
-	gameRooms.push_back(new Room(MainDeck));
-	gameRooms.push_back(new Room(LivingRoom));
+	//gameRooms.clear();
+	//gameRooms.push_back(new Room(Bedroom));
+	//gameRooms.push_back(new Room(Kitchen));
+	//gameRooms.push_back(new Room(MainDeck));
+	//gameRooms.push_back(new Room(LivingRoom));
 
-	for (Room* i : gameRooms) {
-		printSingleBar();
-		i->print();
-		printRoom(*i);
-		system("pause;");
-	}
+	//for (Room* i : gameRooms) {
+	//	printSingleBar();
+	//	i->print();
+	//	printRoom(*i);
+	//	system("pause;");
+	//}
 
 	//Test drive creating characters
-	clearScreen();
-	printTitleBar("\tGame testing: Characters");
-	gameCharacters.push_back(new Character("Alex", Glass, true));
-	gameCharacters.push_back(new Character("Adriana", Glass, false));
-	gameCharacters.push_back(new Character("Ben", Knife, true));
-	gameCharacters.push_back(new Character("Beth", Knife, false));
-	gameCharacters.push_back(new Character("Cam", Pillow, true));
-	gameCharacters.push_back(new Character("Courtney", Pillow, false));
-	gameCharacters.push_back(new Character("Denis", Pills, true));
-	gameCharacters.push_back(new Character("Daniella", Pills, false));
-	gameCharacters.push_back(new Character("Eric", Bottle, true));
-	gameCharacters.push_back(new Character("Ella", Bottle, false));
-	gameCharacters.push_back(new Character("Fred", HairBrush, true));
-	gameCharacters.push_back(new Character("Francesca", HairBrush, false));
-	system("pause");
-	for (Character* i : gameCharacters) {
-		printSingleBar();
-		i->print();
-		system("pause;");
-	}
-	printTitleBar("\t Character combinations Printed");
-	system("pause");
+	//clearScreen();
+	//printTitleBar("\tGame testing: Characters");
+	//gameCharacters.push_back(new Character("Alex", Glass, true));
+	//gameCharacters.push_back(new Character("Adriana", Glass, false));
+	//gameCharacters.push_back(new Character("Ben", Knife, true));
+	//gameCharacters.push_back(new Character("Beth", Knife, false));
+	//gameCharacters.push_back(new Character("Cam", Pillow, true));
+	//gameCharacters.push_back(new Character("Courtney", Pillow, false));
+	//gameCharacters.push_back(new Character("Denis", Pills, true));
+	//gameCharacters.push_back(new Character("Daniella", Pills, false));
+	//gameCharacters.push_back(new Character("Eric", Bottle, true));
+	//gameCharacters.push_back(new Character("Ella", Bottle, false));
+	//gameCharacters.push_back(new Character("Fred", HairBrush, true));
+	//gameCharacters.push_back(new Character("Francesca", HairBrush, false));
+	//system("pause");
+	//for (Character* i : gameCharacters) {
+	//	printSingleBar();
+	//	i->print();
+	//	system("pause;");
+	//}
+	//printTitleBar("\t Character combinations Printed");
+	//system("pause");
 
 
 	gameCharacters.clear();
 	system("cls");
+	int amountOfInnocentPairs = 2;
+
 	vector<string> usedAlibiSection;
+	vector<string> namesToUse = characterNames;
+
+	vector<string> alibisToUse = standardAlibis;
+	int alibisToUseCount = 4;
+
+	int characterObjectKindCount = 6;
+
+	srand(unsigned(time(NULL)));
+	int randomNumber = rand() % amountOfInnocentPairs;
+	int randObject = rand() % characterObjectKindCount;
+
 	printTitleBar("\tGame Loading: Characters");
-for (int i = 0; i < 4; i++) {
-		if (i == 5) {
-			string nameC = gameCharacters[0]->getName(); //random character between 1 and 4
+for (int i = 0; i < (amountOfInnocentPairs * 2) + 1; i++) {
+
+		if (i == amountOfInnocentPairs * 2) {
+			string nameC = gameCharacters[randomNumber]->getName(); //random character between 1 and 4
 			string alibiC = usedAlibiSection[0] + " haha "; //number between 1 and 4
-			gameCharacters.push_back(new Character("ERR3", Glass, false));
+			int randName = rand() % sizeof(namesToUse) + 1;
+			gameCharacters.push_back(new Character(namesToUse[randName], characterObjectKind(randObject), false));
 			gameCharacters.back()->setAlibi(nameC + " and i were " + alibiC);
 		}
 		else if (i % 2 == 0) {
-			//need name logic
-   			string nameA = "A";
-			string nameB = "B";
+			int randName = rand() % sizeof(namesToUse) + 1;
+   			string nameA = namesToUse[randName];
+			namesToUse.erase(namesToUse.begin() + randName);
+			int randNameB = rand() % sizeof(namesToUse) + 1;
+			string nameB = namesToUse[randNameB];
+			namesToUse.erase(namesToUse.begin() + randNameB);
 			//need alibi logic
-			string pairedAlibi = "doing x and y";
+			int randAlibi = rand() % sizeof(alibisToUseCount);
+			string pairedAlibi = alibisToUse[randAlibi];
 			usedAlibiSection.push_back(pairedAlibi);
+			alibisToUse.erase(alibisToUse.begin() + randAlibi);
+			alibisToUseCount -= 1;
 			//make characters with alibi
-			gameCharacters.push_back(new Character("ERR1", Glass, false));
+			gameCharacters.push_back(new Character(nameB, characterObjectKind(randObject), false));
 			gameCharacters.back()->setAlibi(nameA + " and i were " + pairedAlibi);
-			gameCharacters.push_back(new Character("ERR2", Glass, false));
+			gameCharacters.push_back(new Character(nameA, characterObjectKind(randObject), false));
 			gameCharacters.back()->setAlibi(nameB + " and i were " + pairedAlibi);
 		}
 	}
 	system("pause");
 	for (Character* i : gameCharacters) {
 		printSingleBar();
+		cout << randomNumber + "\n";
 		cout << i->getName() + " | " + i->getAlibi();
 		system("pause;");
 	}
